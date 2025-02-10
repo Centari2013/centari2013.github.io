1b300dff928deaf952a07ae25e25b49091ed305b
Zaria Burton <zaria.burton2000@gmail.com>
2025-02-09 22:29:40 -0500

    preliminary browser update

--- src/components/views/BrowserView.vue
+++ src/components/views/BrowserView.vue
@@ -1,7 +1,21 @@
 <template>
-  <div class="h-full w-full">
-    <div class="top-0 h-12"></div>
-    <iframe src="https://centari2013.github.io/FSEA/"  class="h-full w-full"></iframe>
+  <div class="h-full w-full overflow-scroll">
+    <div class="bg-primary-shadow top-0 h-12 flex items-center justify-center sticky">
+      <input class="w-2/3 text-center p-1" :disabled="true" :placeholder="currentUrl"/>
+    </div>
+    
+    <iframe :src="currentUrl" class="h-full w-full"></iframe>
   </div>
   
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      currentUrl: "https://centari2013.github.io/FSEA/",
+      FSEA: "https://centari2013.github.io/FSEA/",
+    }
+  }
+}
+</script>
