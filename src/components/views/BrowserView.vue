1a22723b18be73287802fae4ec4af6570fcad51d
Zaria Burton <zaria.burton2000@gmail.com>
2025-06-21 23:58:45 -0400

    integrated new broswer UI

--- src/components/views/BrowserView.vue
+++ src/components/views/BrowserView.vue
@@ -2,25 +2,28 @@
   <div class="flex flex-col h-full w-full overflow-hidden">
     <!-- Top Bar -->
     <div class="bg-primary-shadow flex items-center justify-between px-2 h-12 shrink-0 border-b border-primary-glow">
-      <div class="flex items-center gap-2 w-full">
+      <div class="flex items-center gap-2 w-full pt-1">
 
         <!-- Back button -->
-        <button class="browser-btn" @click="goBack">
+        <button class="browser-button" @click="goBack">
           <BrowserBackIcon class="browser-icon"/>
         </button>
 
         <!-- Reload button -->
-        <button class="browser-btn" @click="reloadPage">⟳</button>
+        <button class="browser-button" @click="reloadPage">
+          <BrowserRefreshIcon class="browser-icon refresh"/>
+        </button>
 
         <!-- URL Display -->
         <input
           v-model="currentUrl"
           class="bg-gray-900 text-sm text-white px-3 py-1 rounded border border-gray-600 w-full truncate"
+          :disabled="true"
         />
       </div>
 
       <!-- Open in real browser -->
-      <button class="browser-btn ml-2" @click="openInNewTab">
+      <button class="browser-button ml-2" @click="openInNewTab">
         <BrowserNewTabIcon class="browser-icon"/>
       </button>
     </div>
@@ -36,6 +39,8 @@ import { ref } from 'vue';
 import { extractAndDecodeBase64 } from '@/components/utilities/decode'
 import BrowserBackIcon from '@/assets/icons/browserBack.svg'
 import BrowserNewTabIcon from '@/assets/icons/browserNewTab.svg'
+import BrowserRefreshIcon from '@/assets/icons/browserRefresh.svg'
+
 const props = defineProps({
   args: {
     type: Object,
@@ -43,7 +48,7 @@ const props = defineProps({
   }
 })
 
-const currentUrl = ref("https://centari2013.github.io/");
+const currentUrl = ref("https://centari2013.github.io/SoundRoom");
 
 if (props.args) {
   const propLink = extractAndDecodeBase64(props.args.url)
@@ -63,11 +68,11 @@ const openInNewTab = () =>{
 .browser-icon line,
 .browser-icon polyline,
 .browser-icon polygon {
-  @apply stroke-primary-accent-light;
+  @apply stroke-white;
 }
 
 .browser-icon path {
-  @apply fill-primary-accent-light;
+  @apply fill-white;
 }
 
 .browser-icon {
@@ -89,7 +94,12 @@ const openInNewTab = () =>{
   @apply stroke-alerts-base fill-alerts-base;
 }
 
+.refresh path,
+.refresh:hover path {
+  @apply fill-none;
+}
+
 .browser-button {
-  @apply w-1/5 p-0 aspect-square bg-transparent flex items-center justify-center rounded-none rotate-90 border-none;
+  @apply w-1/5 p-0 aspect-square bg-transparent flex items-center justify-center rounded-none border-none;
 }
 </style>
